b0c4963b508ba7010539d12ae350e17eaf53779a
Jay Runkel <jayrunkel@gmail.com>
2017-01-07 14:34:43 -0500

    R example working
    Completed example of pulling data from MongoDB, creating correlation
    Matrix, and writing the matrix back to MongoDB

---
+++ R/corMatrix.r
@@ -0,0 +1,26 @@
+# Spark 1.6
+# ./bin/sparkR --conf "spark.mongodb.input.uri=mongodb://admin:admin@prometheus-0.genband-poc.4183.mongodbdns.com/counties.paCensus?authSource=admin" --conf "spark.mongodb.output.uri=mongodb://admin:admin@prometheus-0.genband-poc.4183.mongodbdns.com/counties.testR?authSource=admin" --packages org.mongodb.spark:mongo-spark-connector_2.10:0.1
+
+# Spark 2.0
+# ./bin/sparkR --conf "spark.mongodb.input.uri=mongodb://admin:admin@prometheus-0.genband-poc.4183.mongodbdns.com/counties.census?authSource=admin" --conf "spark.mongodb.output.uri=mongodb://admin:admin@prometheus-0.genband-poc.4183.mongodbdns.com/counties.census?authSource=admin" --packages org.mongodb.spark:mongo-spark-connector_2.11:1.1.0
+
+# sc <- sparkR.init(appName="test")
+# sqlContext <- sparkRSQL.init(sc)
+
+# Install statistical packages. Only needs to be done once.
+# install.packages("Hmisc") //takes a long time. downloads and compiles a lot of stuff
+
+# Read data from MongoDB in Spark Dataframe
+censusDF <- read.df(sqlContext,
+                    source = "com.mongodb.spark.sql.DefaultSource",
+		    uri = "mongodb://admin:admin@prometheus-0.genband-poc.4183.mongodbdns.com/counties.paCensus?authSource=admin")
+
+
+# spark dataframe to R dataframe
+sparkDF <- collect(censusDF)
+
+row.names(sparkDF) <- sparkDF[["county"]]
+stringCols <- c("county", "_id")
+corReady <- sparkDF[ , !(names(sparkDF) %in% stringCols)]
+
+corMatrix <- cor(corReady)
---
+++ vDemo/shared/corMatrix.scala
@@ -0,0 +1,1 @@
+../../scala/corMatrix.scala
